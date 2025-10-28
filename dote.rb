d5f0c577538d10959e2772d4054dce2014b6379d
kmcwakana <kmc2544@kamiyama.ac.jp>
2025-10-28 11:27:58 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -1,5 +1,5 @@
 class Dote < Formula
-  desc "dot e command"
+  desc "dots e command"
   homepage "https://github.com/kmcwakana/homebrew-dote"
   url "https://raw.githubusercontent.com/kmcwakana/homebrew-dote/main/dote.c"
   sha256 "3ef099f5973f2bb3bd3c38d8a7fc1ca6a9956fde52822f829e81647e9c1f09e1E"
@@ -7,6 +7,6 @@ class Dote < Formula
 
   def install
     system ENV.cc, "dote.c", "-o", "dote"
-    bin.install "dote"
+    bin.install "dotes"
   end
 end
